ac6884855feac708f346813882a8f917c1e51238
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-05-05 00:11:31 +0530

    Optimize kth largest element retrieval method
    Refactor largestElement function to use a priority queue for better performance.

--- 215_kth_largest_element.cpp
+++ 215_kth_largest_element.cpp
@@ -7,11 +7,24 @@ class Solution
 {
 public:
 	int largestElement(vector<int>& nums, int k){
-		sort(nums.begin(), nums.end(), greater<int>());
-		
-		return nums[k-1];
+		//35ms
+		//sort(nums.begin(), nums.end(), greater<int>());
+		//return nums[k-1];
+
+		//43ms
+		priority_queue<int, vector<int>, greater<int>> pq;
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            
+            pq.push(nums[i]);
+
+            if(pq.size() > k){
+                pq.pop();
+            }
+        } 
+        return pq.top();
 	}
-	
 };
 
 int main()
