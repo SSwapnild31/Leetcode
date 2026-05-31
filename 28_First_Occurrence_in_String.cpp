04159b283a84b71911bd23bc1cb293af800fc48c
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-05-31 23:03:45 +0530

    Simplify if statements and loop structure

--- 28_First_Occurrence_in_String.cpp
+++ 28_First_Occurrence_in_String.cpp
@@ -5,19 +5,15 @@ public:
         int m = haystack.size();
         int n = needle.size();
 
-        if(n > m)   return -1;
+        if(n > m) return -1;
 
         for(int i=0; i<m; i++){
             if(needle[0] == haystack[i]){
                 int j = 1;
                 for(j=1; j<n; j++){
-                    if(haystack[i+j] != needle[j]){
-                        break;
-                    }
+                    if(haystack[i+j] != needle[j]) break;
                 }
-                if(needle[j] == '\0'){
-                    return i;
-                }
+                if(needle[j] == '\0') return i;
             }
         }
         return -1;
